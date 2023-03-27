ddc36c34b743625566c6166a23072734e58b8e25
ygm <1079462432@qq.com>
2023-03-27 20:13:18 +0800

    2023-3-27-20-13

--- Basic knowledge/helloworld.c
+++ Basic knowledge/helloworld.c
@@ -1,10 +1,26 @@
-#include <stdio.h>
+#include<stdio.h>
+
+int var[] = {1,2,3,4,5,6,7,8,9};
+int size = sizeof(var)/sizeof(var[0]);
+double avg;
+
+double average(int *arr, int size);
+
 int main()
 {
-    int var_runoob = 10;
-    int *p;
-    p = &var_runoob;
-    printf("var_runoob = %p", p);
-    return 0;
 
+    avg = average(var, size);
+    printf("The average is %f, size is %d\n", avg, size);
+
+}
+
+double average(int *arr, int size)
+{
+    int i;
+    double sum = 0;
+    for(i = 0; i < size; i++)
+    {
+        sum += arr[i];
+    }
+    return (double)(sum/size);
 }
